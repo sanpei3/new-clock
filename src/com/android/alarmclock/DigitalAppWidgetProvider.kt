332ce6fd3935f2b2f616ef8dac47ac498af3299a
sanpei3 <sanpei@sanpei.org>
2021-12-30 00:12:38 +0000

    fix packagename from com.android -> com.best

--- src/com/android/alarmclock/DigitalAppWidgetProvider.kt
+++ src/com/android/alarmclock/DigitalAppWidgetProvider.kt
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.android.alarmclock
+package com.best.alarmclock
 
 import android.annotation.SuppressLint
 import android.app.AlarmManager
